9d7dcafbda823b29bbf0df1b943282bdb9c6287c
RunKaRin <dlrjswnrla@naver.com>
2024-01-11 20:00:13 +0900

    2024-01-11 20:00, 로그백

--- 01/api/src/main/java/org/springboot/api/controller/GetController.java
+++ 01/api/src/main/java/org/springboot/api/controller/GetController.java
@@ -2,6 +2,8 @@ package org.springboot.api.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springboot.api.dto.MemberDto;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,18 +13,23 @@ import java.util.Map;
 @RequestMapping("/api/v1/get-api")
 public class GetController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String getHello(){
+    public String getHello() {
+        LOGGER.info("getHello 메서드가 호출되었습니다.");
         return "Hello World";
     }
 
     @GetMapping(value = "/name")
     public String getName() {
+        LOGGER.info("getName 메서드가 호출되었습니다.");
         return "Geon Joo";
     }
 
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable) {
+        LOGGER.info("@PathVariable을 통해 들어온 값 : {}", variable);
         return variable;
     }
 
