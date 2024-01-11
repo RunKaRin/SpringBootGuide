7d803f4a9b57b9a1119873e6d6c6269cf6909b92
RunKaRin <dlrjswnrla@naver.com>
2024-01-11 18:24:59 +0900

    Put Controller

--- 01/api/src/main/java/org/springboot/api/controller/PutController.java
+++ 01/api/src/main/java/org/springboot/api/controller/PutController.java
@@ -1,6 +1,9 @@
 package org.springboot.api.controller;
 
 import org.springboot.api.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +34,10 @@ public class PutController {
     @PutMapping(value = "/member2")
     public MemberDto postMemberDto2(@RequestBody MemberDto memberDto) {
         return memberDto;
+    }
+
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDto> postMemberDto3(@RequestBody MemberDto memberDto) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(memberDto);
     }
 }
