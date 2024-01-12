3ddebaaa7e60a7fa9d556c63357337637b7b462c
RunKaRin <dlrjswnrla@naver.com>
2024-01-13 00:05:15 +0900

    2024-01-13 00:04, 엔티티 어노테이션 추가

--- 01/jpa/src/main/java/org/springboot/jpa/data/entity/Product.java
+++ 01/jpa/src/main/java/org/springboot/jpa/data/entity/Product.java
@@ -1,11 +1,16 @@
 package org.springboot.jpa.data.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @Entity
 @Data
+@NoArgsConstructor   // 파라미터가 없는 기본 생성자 생성
+@AllArgsConstructor  // 모든 필드를 인자로 받는 생성자 생성
 @Table(name = "product")
 public class Product {
 
