3209fbd7e18f677c42d4c9cc4bcf40c9fdafa39c
이건주 <137998128+RunKaRin@users.noreply.github.com>
2024-01-13 17:12:54 +0900

    Delete test directory

--- test/src/main/java/org/springboot/test/data/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package org.springboot.test.data.repository;
-
-import org.springboot.test.data.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
