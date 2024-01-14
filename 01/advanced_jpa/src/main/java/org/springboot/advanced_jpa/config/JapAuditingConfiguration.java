4694e3caace8bbda3e1c77f8412948a02c6bd74d
RunKaRin <dlrjswnrla@naver.com>
2024-01-15 02:21:49 +0900

    2024.01.15 02:21 / JPA Auditing 걸러 테스트코드 바꾸기귀찮아

--- 01/advanced_jpa/src/main/java/org/springboot/advanced_jpa/config/JapAuditingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.springboot.advanced_jpa.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JapAuditingConfiguration {
-}
