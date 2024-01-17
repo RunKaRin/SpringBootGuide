efce7ee87e4cb27fa3513a165ead888fe4dc5721
RunKaRin <dlrjswnrla@naver.com>
2024-01-17 15:22:56 +0900

    2024.01.17 15:22 / 서비스 추가

---
+++ 01/rest/src/main/java/org/springboot/rest/service/RestTemplateService.java
@@ -0,0 +1,56 @@
+package org.springboot.rest.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class RestTemplateService {
+
+    public String getName() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9000")
+                .path("/crud-api")
+                .encode()
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+
+        return responseEntity.getBody();
+    }
+
+    public String getNameWithPathVariable() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9000")
+                .path("/crud-api/{name}")
+                .encode()
+                .build()
+                .expand("KaRin")
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+
+        return responseEntity.getBody();
+    }
+
+    public String getNameWithParameter() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9000")
+                .path("crud-api/param")
+                .queryParam("name", "KaRin")
+                .encode()
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+
+        return responseEntity.getBody();
+    }
+}
